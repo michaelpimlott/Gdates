c9cebbb827c742e17b3959388b0ab8b56eab552b
Michael Pimlott <pettingbutterscotch@gmail.com>
2016-05-19 17:35:24 -0700

    add controllers in app.js

--- js/app.js
+++ js/app.js
@@ -7,4 +7,24 @@
     ]);
     .config(config);
 
+  function config($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/home.html',
+        controller: 'HomeController'
+      })
+      .when('/members', {
+        templateUrl: 'partials/members.html',
+        controller: 'membersController'
+      })
+      .when('/profile', {
+        templateUrl: 'partials/profile.html',
+        controller: 'ProfileController'
+      })
+      .when('/search', {
+        templateUrl: 'partials/search.html',
+        controller: 'SearchController'
+      })
+  };
+
   }());
